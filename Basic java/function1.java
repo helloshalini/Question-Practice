57f5476724515c1006e46f98107fd10e4bb81f0a
helloshalini <shalinijaiswal1101@gmail.com>
2025-05-02 22:55:16 +0530

    Average of a number

---
+++ Basic java/function1.java
@@ -0,0 +1,13 @@
+public class function1 {
+    public static void average(int a, int b, int c) {
+        int avg = (a+b+c)/3;
+        System.out.println(avg);
+    }
+
+    public static void main(String[] args) {
+        average(4, 4, 10);
+
+    }
+    
+}
+
