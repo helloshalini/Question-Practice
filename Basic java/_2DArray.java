88dc49022513cc13abe1d299607002d6c13acc99
helloshalini <shalinijaiswal1101@gmail.com>
2025-05-24 18:16:32 +0530

    Array

--- Basic java/_2DArray.java
+++ Basic java/_2DArray.java
@@ -1,10 +1,11 @@
 public class _2DArray {
     public static void main(String[] args) {
-        int[][] array = { {4,7,8},{9,14,7} };
+        int[][] array = { {4,7,8},
+                          {9,14,7} };
         int count = 0;
-        for(int i = 0; i<array.length-1; i++) {
-            for(int j = 0; j<array[0].length-1; j++) {
-                if(array[i][j] == 8) {
+        for(int i = 0; i<array.length; i++) {
+            for(int j = 0; j<array[0].length; j++) {
+                if(array[i][j] == 7) {
                     count ++;
                 }
             }
