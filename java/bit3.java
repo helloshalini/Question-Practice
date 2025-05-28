6c2b3a75282f1045058a1356259406a34fb70ed3
helloshalini <shalinijaiswal1101@gmail.com>
2025-05-28 23:51:02 +0530

    Add 1 in bit number

---
+++ java/bit3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class bit3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.println(x+" + "+1+" is "+ -~x);
+
+        sc.close();
+    }
+}
